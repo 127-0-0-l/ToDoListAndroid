881f17edfaddb545b952a9a534bdd1bd4acc9917
Maksim <borisovets.maksim@gmail.com>
2021-07-13 20:55:42 +0300

    add project files

---
+++ ToDoList/ToDoList/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Application.Current.UserAppTheme = OSAppTheme.Light;
+        }
+
+        private void ibtnThemeSwitcher_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNewTask_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
